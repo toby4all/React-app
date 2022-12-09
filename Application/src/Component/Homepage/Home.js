3a9b8a637e49fd0e0c0758073f26831d9ad0e5be
Fabonmi <fabonmitobi@gmail.com>
2022-12-10 02:55:42 +0400

    adde more futures to my application

--- Application/src/Component/Homepage/Home.js
+++ Application/src/Component/Homepage/Home.js
@@ -6,12 +6,12 @@ function Home(){
         <div className="divContainer">
             <div>
             <h3 id="h3">Welcome to React Technologies</h3>
-          <p id="p1">
+          <p className="p1">
             Dive into the digital World with applications built with react.
             The best technology for your UI.
           </p>
             </div>
-            <button id="btn">click her to Get started </button>
+            <button className="btn">click her to Get started </button>
         </div>
     )
 }
