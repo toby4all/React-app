baa1b2cb3b8f1eb4846457d616216476e8e1196c
Fabonmi <fabonmitobi@gmail.com>
2022-12-22 05:14:59 +0400

    collected information from MondoDb database

---
+++ MongoDB/NewSeverApp/app.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var mongodb= require("mongodb");
+var cors= require("cors");
+ var mongoClient=  mongodb.MongoClient;
+
+var dburl= "mongodb+srv://tobby123:Meandsunday08@cluster0.bu7qdsk.mongodb.net/?retryWrites=true&w=majority";
+Route= express();
+Route.use(cors());
+
+Route.get('/productdb', (req, res)=>{
+mongoClient.connect(dburl, (err, cluster)=>{
+    if(err){
+        res.send('Error while connecting to the database')
+        console.log(err)
+    }else {
+        dbRef= cluster.db("ProductDB");
+        var collectonRef= 
+        dbRef.collection("electronicCollection");
+        collectonRef.find().toArray((error, data)=>{
+            if(error){
+                res.send('Error fetching documented data');
+
+            }else{
+                res.json({
+                    results:data,
+                    ok:true,
+                });
+            }
+        });
+    }
+});
+  console.log('database information loaded in the terminal');
+});//pathh will be http://localhost:4001/productdb
+
+Route.listen(4001,()=>{
+    console.log('Server started')
+});
